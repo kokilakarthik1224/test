9e66269053d11063bfec632064cdfe70eef9e9a7
kokilakarthik1224 <kokilakarthik12@gmail.com>
2025-12-26 11:12:28 +0530

    files added

---
+++ test.py
@@ -0,0 +1,8 @@
+Python 3.13.5 (tags/v3.13.5:6cb20a2, Jun 11 2025, 16:15:46) [MSC v.1943 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> a=input()
+b=int input()
+>>> print ("My name is=(a)")
+My name is=(a)
+>>> print ("My age is (b)")
+My age is (b)
---
+++ test1.py
@@ -0,0 +1,6 @@
+fan=[20,10,30]
+tab=[1,3]
+tab.extend(fan)
+print(fan,tab)
+
+
